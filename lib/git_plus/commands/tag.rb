fcdd6e70e0f0ddad64ab80d34047131f4f8a68fb
Brooke Kuhlmann <brooke@alchemists.io>
2020-12-28 15:08:58 -0700

    Added tag command
    Necessary to provide information about tags.

---
+++ lib/git_plus/commands/tag.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "open3"
+require "tempfile"
+
+module GitPlus
+  module Commands
+    # A Git tag command wrapper.
+    class Tag
+      def initialize shell: Open3
+        @shell = shell
+      end
+
+      def call *arguments
+        shell.capture3 "git", "tag", *arguments
+      end
+
+      def exist? version
+        local?(version) || remote?(version)
+      end
+
+      def last
+        shell.capture3("git", "describe", "--abbrev=0", "--tags", "--always")
+             .then { |stdout, _stderr, status| status.success? ? stdout.strip : "" }
+      end
+
+      def local? version
+        call("--list", version).then do |stdout, _stderr, status|
+          status.success? && stdout.match?(/\A#{version}\Z/)
+        end
+      end
+
+      def push
+        shell.capture3 "git", "push", "--tags"
+      end
+
+      def remote? version
+        shell.capture3("git", "ls-remote", "--tags", "origin", version)
+             .then do |stdout, _stderr, status|
+               status.success? && stdout.match?(%r(.+tags/#{version}\Z))
+             end
+      end
+
+      def sign version, body = ""
+        create version, body, %w[--sign]
+      end
+
+      def tagged?
+        call.then { |stdout, _stderr, status| status.success? && !stdout.empty? }
+      end
+
+      def unsign version, body = ""
+        create version, body, %w[--no-sign]
+      end
+
+      private
+
+      attr_reader :shell
+
+      def create version, body, options = []
+        fail Errors::Base, "Unable to create Git tag without version." unless version
+        fail Errors::Base, "Tag exists: #{version}." if exist? version
+
+        Tempfile.open Identity::NAME do |file|
+          file.write body
+          write version, file.tap(&:rewind), options
+        end
+      end
+
+      def write version, file, options = []
+        arguments = ["--annotate", version, "--cleanup", "verbatim", *options, "--file", file.path]
+
+        call(*arguments).then do |_stdout, _stderr, status|
+          fail Errors::Base, "Unable to create tag: #{version}." unless status.success?
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/git_plus/commands/tag_spec.rb
@@ -0,0 +1,172 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe GitPlus::Commands::Tag do
+  subject(:tag) { described_class.new }
+
+  include_context "with Git repository"
+
+  using Refinements::Pathnames
+
+  shared_examples_for "a tag" do |method|
+    it "fails when there is no version" do
+      expectation = proc { tag.public_send method, nil }
+
+      expect(&expectation).to raise_error(
+        GitPlus::Errors::Base,
+        "Unable to create Git tag without version."
+      )
+    end
+
+    it "prints warning when tag exists" do
+      git_repo_dir.change_dir do
+        `git tag 0.1.0`
+        expectation = proc { tag.public_send method, "0.1.0" }
+
+        expect(&expectation).to raise_error(GitPlus::Errors::Base, "Tag exists: 0.1.0.")
+      end
+    end
+  end
+
+  describe "#call" do
+    it "answers standard output, standard error, and status" do
+      git_repo_dir.change_dir do
+        expect(tag.call("--list")).to match(
+          array_including("", "", kind_of(Process::Status))
+        )
+      end
+    end
+  end
+
+  describe "#exist?" do
+    it "answers true when only local tag exists" do
+      git_repo_dir.change_dir do
+        `git tag 0.0.0`
+        expect(tag.exist?("0.0.0")).to eq(true)
+      end
+    end
+
+    it "answers true when only remote tag exists" do
+      git_repo_dir.change_dir do
+        expect(tag.exist?("0.1.0")).to eq(true)
+      end
+    end
+
+    it "answers false when local and remote tags don't exist" do
+      git_repo_dir.change_dir do
+        expect(tag.exist?("0.0.0")).to eq(false)
+      end
+    end
+  end
+
+  describe "#last" do
+    it "answers SHA of first commit when no tags exist" do
+      git_repo_dir.change_dir do
+        expect(tag.last).to match(/[0-9a-f]{40}/)
+      end
+    end
+
+    it "answers last tag when tag exists" do
+      git_repo_dir.change_dir do
+        `touch test.txt && git add . && git commit --message "Added test file" && git tag 0.1.0`
+        expect(tag.last).to eq("0.1.0")
+      end
+    end
+  end
+
+  describe "#local?" do
+    it "answers true with matching tag" do
+      git_repo_dir.change_dir do
+        `git tag 0.1.0`
+        expect(tag.local?("0.1.0")).to eq(true)
+      end
+    end
+
+    it "answers false with non-existant tag" do
+      git_repo_dir.change_dir do
+        `git tag 0.0.0`
+        expect(tag.local?("0.1.0")).to eq(false)
+      end
+    end
+
+    it "answers false with no tags" do
+      git_repo_dir.change_dir do
+        expect(tag.local?("0.1.0")).to eq(false)
+      end
+    end
+  end
+
+  describe "#push" do
+    subject(:tag) { described_class.new shell: shell }
+
+    let(:shell) { class_spy Open3 }
+
+    it "pushes tags" do
+      git_repo_dir.change_dir do
+        `git tag 0.1.0`
+        tag.push
+
+        expect(shell).to have_received(:capture3).with("git", "push", "--tags")
+      end
+    end
+  end
+
+  describe "#remote?" do
+    subject(:tag) { described_class.new shell: shell }
+
+    let(:shell) { class_spy Open3, capture3: ["refs/tags/0.1.0", "", status] }
+    let(:status) { instance_spy Process::Status, success?: true }
+
+    it "answers true when tag exists" do
+      git_repo_dir.change_dir do
+        expect(tag.remote?("0.1.0")).to eq(true)
+      end
+    end
+
+    it "answers false when tag doesn't exist" do
+      git_repo_dir.change_dir do
+        expect(tag.remote?("0.2.0")).to eq(false)
+      end
+    end
+  end
+
+  describe "#sign" do
+    it_behaves_like "a tag", :sign
+
+    it "creates signed tag" do
+      skip "Temporarily disabled on CI" if ENV["CI"] == "true"
+
+      git_repo_dir.change_dir do
+        tag.sign "0.0.0"
+        expect(`git show --stat --pretty=format:"%b" "0.0.0"`).to match(/tag 0.0.0.+SIGNATURE/m)
+      end
+    end
+  end
+
+  describe "#tagged?" do
+    it "answers true when tags exist" do
+      git_repo_dir.change_dir do
+        `git tag 0.1.0`
+        expect(tag.tagged?).to eq(true)
+      end
+    end
+
+    it "answers false when tags don't exist" do
+      git_repo_dir.change_dir do
+        expect(tag.tagged?).to eq(false)
+      end
+    end
+  end
+
+  describe "#unsign" do
+    it_behaves_like "a tag", :unsign
+
+    it "creates unsigned tag" do
+      git_repo_dir.change_dir do
+        tag.unsign "0.0.0"
+        expect(`git show --stat --pretty=format:"%b" "0.0.0"`).to match(/tag 0.0.0/m)
+      end
+    end
+  end
+end
