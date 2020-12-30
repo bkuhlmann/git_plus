38b37b1490f2232bf26abc4d300be11cc8d2ba11
Brooke Kuhlmann <brooke@alchemists.io>
2020-12-29 17:33:01 -0700

    Added version release notes

--- lib/git_plus/identity.rb
+++ lib/git_plus/identity.rb
@@ -4,7 +4,7 @@ module GitPlus
   # Gem identity information.
   module Identity
     NAME = "git_plus"
-    LABEL = "Git Plus"
+    LABEL = "Git+"
     VERSION = "0.1.0"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
   end
